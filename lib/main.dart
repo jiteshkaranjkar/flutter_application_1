b3802e193bc9d93df53e52d41caffcaee906f0de
JK <jiteshkaranjkar@gmail.com>
2024-06-27 19:04:21 +1000

    Add nav, state for Favs

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -43,7 +44,7 @@ class MyAppState extends ChangeNotifier {
     } else {
       favorites.add(current);
     }
-  notifyListeners();
+    notifyListeners();
   }
 }
 
@@ -55,12 +56,94 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int selectedIndex = 0;
 
+
+  @override
+  Widget build(BuildContext context) {
+  Widget page;
+  switch (selectedIndex) {
+    case 0:
+      page = GeneratorPage();
+      break;
+    case 1:
+      page = FavoritesPage();
+      break;
+    default:
+      throw UnimplementedError('no widget for $selectedIndex');
+  }
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        return Scaffold(
+          body: Row(
+            children: [
+              SafeArea(
+                child: NavigationRail(
+                  extended: constraints.maxWidth >= 400,
+                  destinations: [
+                    NavigationRailDestination(
+                      icon: Icon(Icons.home),
+                      label: Text('Home'),
+                    ),
+                    NavigationRailDestination(
+                      icon: Icon(Icons.favorite),
+                      label: Text('Favorites'),
+                    ),
+                  ],
+                  selectedIndex: selectedIndex,
+                  onDestinationSelected: (value) {
+                    setState(() {
+                      selectedIndex = value;
+                    });
+                  },
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Theme.of(context).colorScheme.primaryContainer,
+                  child: page,
+                ),
+              ),
+            ],
+          ),
+        );
+      }
+    );
+  }
+}
+
+class FavoritesPage extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context){
+    var appState = context.watch<MyAppState>();
+    if(appState.favorites.isEmpty){
+      return Center(
+        child: Text('No favorites yet.'),
+      );
+    }
+
+    return ListView(
+      children : [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('You have ${appState.favorites.length} favorites.'),
+        ),
+        for( var pair in appState.favorites)
+          ListTile(
+            leading: Icon(Icons.favorite),
+            title: Text(pair.asLowerCase),
+          ),
+      ],
+    );
+  }
+}
+
+class GeneratorPage extends StatelessWidget{
+  int _counter = 0;
+  
   void _incrementCounter() {
-    setState(() {
       _counter++;
-    });
   }
 
   @override
@@ -78,10 +161,11 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text("widget.title"),
       ),
       body: Center(
-        child: Column(
+        child: 
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             BigCard(pair: pair),
@@ -111,47 +195,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }
-}
-
-class GeneratorPage extends StatelessWidget{
-  int _counter = 0;
-
-  @override 
-  Widget build(BuildContext context){
-    var appState = context.watch<MyAppState>();
-    var pair = appState.current;
-
-    IconData icon;
-    if (appState.favorites.contains(pair)) {
-      icon = Icons.favorite;
-    } else {
-      icon = Icons.favorite_border;
-    }
-
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            BigCard(pair: pair),
-            SizedBox(height: 30,),
-            Row(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                ElevatedButton.icon(
-                  onPressed: () {
-                    appState.toggleFavorite();
-                  },
-                  icon: Icon(icon),
-                  label: Text("Like"),
-                  ),
-                  SizedBox(width: 20,),
-                BigCounter(counter: _counter),
-              ],
-            ),
-          ],
-      )
     );
   }
 }
